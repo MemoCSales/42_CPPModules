ae2f7b3fd41a77758213300ed80c8d1bafe12312
Manuel Guillermo Cruz Sales <mcruz-sa@c2r5s5.42wolfsburg.de>
2024-08-15 17:41:37 +0200

    Adding comments in the makefile

--- CPP_01/ex01/main.cpp
+++ CPP_01/ex01/main.cpp
@@ -4,6 +4,7 @@ int main() {
     Zombie  zombie4;
     int     n = 10;
     Zombie  *zombie5 = zombie4.zombieHorde(n, "Bob");
+    std::cout << GREEN << "Number of Zombies in the Horde: " << n << DEFAULT << std::endl;
     for (int i = 0; i < n; i++) {
         zombie5[i].announce();
     }
