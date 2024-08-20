c6fdb1de4b2de91ade783c28709050d650151161
Manuel Guillermo Cruz Sales <mcruz-sa@c3r2s3.42wolfsburg.de>
2024-08-20 20:56:47 +0200

    HumanB

--- CPP_01/ex03/HumanB.cpp
+++ CPP_01/ex03/HumanB.cpp
@@ -1,13 +1,12 @@
 #include "HumanB.hpp"
 
-// HumanB::HumanB()
-// {
-// 	if (DEBUG)
-// 		std::cout << "Default constructor called in HumanB class" << std::endl;
-// }
+HumanB::HumanB(std::string _name) : name(_name) {
+	if (DEBUG)
+		std::cout << "Default constructor called in HumanB class" << std::endl;
+}
 
-// HumanB::~HumanB()
-// {
-// 	if (DEBUG)
-// 		std::cout << "Default deconstructor called in HumanB class" << std::endl;
-// }
+HumanB::~HumanB()
+{
+	if (DEBUG)
+		std::cout << "Default deconstructor called in HumanB class" << std::endl;
+}
