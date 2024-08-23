0a387e458583c7305b4a6f36ed527408723a0a99
Manuel Guillermo Cruz Sales <mcruz-sa@c2r6s3.42wolfsburg.de>
2024-08-23 15:00:59 +0200

    ex04 finished

--- CPP_01/ex04/main.cpp
+++ CPP_01/ex04/main.cpp
@@ -1,27 +1,35 @@
 #include "Sed.hpp"
 
-using namespace std;
 /*
-create a program that takes 3 parameters in the following order:
-	filename
-	s1
-	s2
-It will open the file <filename> and copies its content into a new file <filename>.replace --> It will replace every occurence of s1 with s2
+	create a program that takes 3 parameters in the following order:
+		filename
+		s1
+		s2
+	It will open the file <filename> and copies its content into a new file <filename>.replace --> It will replace every occurence of s1 with s2
 */
 int main(int argc, char **argv) {
 	
 	std::string	filename, outFileName, s1, s2;
 	std::string fileExtension = ".replace";
 	std::string lineFile;
-	std::string tempString;
+	std::string originalString;
 
-	if (argc != 4)
+	if (argc != 4) {
 		std::cerr << RED << "\tError: " << DEFAULT << WHITE << " ./program_name filename string1 string2" << DEFAULT << std::endl;
+		exit(EXIT_FAILURE);
+	}
 	
 	filename = argv[1];
 	s1 = argv[2];
 	s2 = argv[3];
 
+	std::size_t lenStr1 = s1.length();
+	std::size_t lenStr2 = s2.length();
+	if (lenStr1 <= 0 || lenStr2 <= 0) {
+		std::cout << "Error: Empty string found. Please try again with valid string input." << std::endl;
+		exit(EXIT_FAILURE);
+	}
+	
 	//Remove the extension from the file, by searching from the end of the string
 	//rfind returns the index of the dot within the stirng.
 	size_t	lastDot = filename.rfind('.');
@@ -29,42 +37,43 @@ int main(int argc, char **argv) {
 	if (lastDot != std::string::npos) {
 		filename = filename.substr(0, lastDot);
 	}
-
-	// std::cout << "Filename: " <<  filename << std::endl;
-	// std::cout << "String1: " <<  s1 << std::endl;
-	// std::cout << "String2: " <<  s2 << std::endl;
 	
 	std::ifstream inFile(argv[1], std::ifstream::in);
-
-	// if (!inFile.is_open()) {
-	// 	// std::cout << "File: " << filename << " opened successfully." << std::endl;
-	// 	std::getline(inFile, lineFile);
-	// 	// std::cout << lineFile << std::endl;
-	// }
-	// else
 
 	filename.append(fileExtension);
 
 	if (inFile.is_open()) {
 		char	c;
 		while (inFile.get(c)) {
-			tempString += c;
+			originalString += c;
 		}
 	}
 	else {
 		std::cerr << "Error opening file. Check filename." << std::endl;
+		exit(EXIT_FAILURE);
 	}
-	std::cout << tempString << std::endl;
 
+	std::size_t	initPos = originalString.find(s1);
+	if (initPos == std::string::npos)
+		std::cout << "No string in the file matches with string1. Try again..." << std::endl;
 
-	std::size_t	found = tempString.find(s1);
-	if (found != std::string::npos)
-		std::cout << "Ocurrence of string: " << s1 << " found at position: " << found << std::endl;
-	else
-		std::cout << "String: " << s1 << " not found." << std::endl;
-
-	
+	size_t	i = 0;
+	std::string concaString;
+	while (originalString[i]) {
+		initPos = originalString.find(s1, i);
+		if (i == initPos) {
+			concaString += s2;
+			i += lenStr1;
+		}
+		else {
+			concaString += originalString[i];
+			i++;
+		}
+	}
 	std::ofstream outFile(filename.c_str(), std::ofstream::out);
+	
+	outFile << concaString;
+
 	outFile.close();
 	inFile.close();
 	return 0;
