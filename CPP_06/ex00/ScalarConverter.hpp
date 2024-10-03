c98f39520316344c06ccfbad15c9ec1d5796ce6c
Manuel Guillermo Cruz Sales <mcruz-sa@c2r7s1.42wolfsburg.de>
2024-10-03 16:28:45 +0200

    progress ex00 cpp06

--- CPP_06/ex00/ScalarConverter.hpp
+++ CPP_06/ex00/ScalarConverter.hpp
@@ -3,6 +3,9 @@
 # include <iostream>
 # include <string>
 # include <iomanip>
+# include <cmath>
+# include <math.h>
+# include <cstdlib> //atoi
 
 # define DEBUG 0
 
@@ -25,10 +28,10 @@ class ScalarConverter {
 
 
 bool isChar(std::string& lit);
-// bool isInt(std::string& lit);
+bool isInt(std::string& lit);
 
 void printChar(std::string& lit);
-// void printInt(std::string& lit);
+void printInt(std::string& lit);
 
 #endif
 
@@ -44,8 +47,8 @@ ScalarConverter::ScalarConverter(void) {
 void ScalarConverter::convert(std::string& literal) {
 	if (isChar(literal))
 		printChar(literal);
-	// else if (isInt(literal))
-	// 	printInt(literal);
+	else if (isInt(literal))
+		printInt(literal);
 }
 
 bool isChar(std::string& lit) {
@@ -55,16 +58,41 @@ bool isChar(std::string& lit) {
 	return false;	
 }
 
+bool isInt(std::string& lit) {
+	if (lit.empty()) {
+		return false;
+	}
+
+	size_t start = 0;
+	if (lit[0] == '+' || lit[0] == '-') {
+		start = 1;
+	}
+	if (start == lit.size()) {
+		return false; // String is just a sign, not a valid number
+	}
+	for (size_t i = start; i < lit.size(); i++)	{
+		if (!isdigit(lit[i])) {
+			return false;
+		}
+	}
+	return true;
+}
+
 void printChar(std::string& lit) {
 	char c = lit[0];
 	std::cout << "char: '" << lit << "'" << std::endl;
 	std::cout << "int: " << static_cast<int>(c) << std::endl;
-	std::cout << std::fixed << std::setprecision(1);
-	std::cout << "float: " << static_cast<float>(c) << std::endl;
+	// std::cout << std::fixed << std::setprecision(1);
+	std::cout << "float: " << static_cast<float>(c) << 'f' << std::endl;
 	std::cout << "double: " << static_cast<double>(c) << std::endl;
 }
 
-
-// bool isInt(std::string& lit) {
-
-// }
+void printInt(std::string& lit) {
+	int num = std::atoi(lit.c_str());
+	std::cout << "char: Non displayable" << std::endl;
+	std::cout << "int: " << num << std::endl;
+	// std::cout << std::fixed << std::setprecision(1);
+	std::cout << "float: " << static_cast<float>(num) << ".0f" << std::endl;
+	std::cout << "double: " << static_cast<double>(num) << ".0" << std::endl;
+}
+// 2147483647
