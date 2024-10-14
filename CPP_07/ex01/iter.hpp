694b419465ed78bbd372363f7989be1aa771a495
Manuel Guillermo Cruz Sales <mcruz-sa@c2r9s8.42wolfsburg.de>
2024-10-14 12:45:50 +0200

    finished test of ex01 cpp07

--- CPP_07/ex01/iter.hpp
+++ CPP_07/ex01/iter.hpp
@@ -1,6 +1,17 @@
 #ifndef ITER_HPP
 # define ITER_HPP
 # include <iostream>
+# include <cctype>
+
+# define RED		"\033[1;31m"
+# define GREEN		"\033[1;32m"
+# define YELLOW		"\033[1;33m"
+# define BLUE		"\033[1;34m"
+# define MAGENT		"\033[1;35m"
+# define CYAN		"\033[1;36m"
+# define WHITE		"\033[1;37m"
+# define DEFAULT		"\033[0m"
+
 
 template <typename T> void printElement(T& element) {
 	std::cout << element << std::endl;
@@ -17,8 +28,24 @@ template <typename T> void iter(T *array, size_t size, void (*func)( T&)) {
 }
 
 // ---------- Callback chars functions ------------
-template <typename T> void toUpper(char& element) {
-	element = std::toupper(char& element);
+template <typename T> void toUpper(T& element) {
+	element = std::toupper(static_cast<int>(element));
+}
+
+void toUpperString(std::string& str) {
+	for (size_t i = 0; i < str.length(); i++)
+	{
+		str[i] = std::toupper(static_cast<int>(str[i]));
+	}
+	
+}
+
+void toLowerString(std::string &str) {
+	for (size_t i = 0; i < str.length(); i++)
+	{
+		str[i] = std::tolower(static_cast<int>(str[i]));
+	}
+	
 }
 
 #endif
--- CPP_07/ex01/main.cpp
+++ CPP_07/ex01/main.cpp
@@ -4,16 +4,26 @@ int main(void) {
 	int arr[] = {1,2,3};
 	int size = 3;
 
+	std::cout << GREEN << "ARRAY OF INTEGERS: " << DEFAULT << std::endl;
 	::iter(arr, size, printElement<int>);
-	std::cout << "Multiply each element of the int array by 2:" << std::endl;
+	std::cout << GREEN << "MULTIPLY EACH ELEMENT BY 2:" << DEFAULT << std::endl;
 	::iter(arr, size, multiplyElement);
 	::iter(arr, size, printElement<int>);
 
 	char array[] = {'a', 'b', 'c'};
 	// int size = 3;
-	std::cout << "Array of chars:" << std::endl;
+	std::cout << GREEN << "ARRAY OF CHARS:" << DEFAULT << std::endl;
 	::iter(array, size, printElement);
 	::iter(array, size, toUpper);
+	::iter(array, size, printElement);
+
+	std::cout << GREEN << "ARRAY OF STRINGS:" << DEFAULT << std::endl;
+	std::string arrayStr[] = {"Hola", "42", "adios"};
+	::iter(arrayStr, size, toUpperString);
+	::iter(arrayStr, size, printElement);
+	::iter(arrayStr, size, toLowerString);
+	::iter(arrayStr, size, printElement);
+
 
 	return 0;
 }
