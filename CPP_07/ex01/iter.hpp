c33b476348cc614689564230c5bea382d4f1a665
MemoCSales <memocsales@gmail.com>
2024-10-14 11:10:50 +0200

    more functions to test

--- CPP_07/ex01/iter.hpp
+++ CPP_07/ex01/iter.hpp
@@ -2,14 +2,23 @@
 # define ITER_HPP
 # include <iostream>
 
-template <typename T> void printElement(const T& element) {
+template <typename T> void printElement(T& element) {
 	std::cout << element << std::endl;
 }
 
-template <typename T> void iter(T *array, size_t size, void (*func)(const T&)) {
+template <typename T> void multiplyElement(T& element) {
+	element *= 2;
+}
+
+template <typename T> void iter(T *array, size_t size, void (*func)( T&)) {
 	for (size_t i = 0; i < size; i++) {
 		func(array[i]);
 	}
+}
+
+// ---------- Callback chars functions ------------
+template <typename T> void toUpper(char& element) {
+	element = std::toupper(char& element);
 }
 
 #endif
--- CPP_07/ex01/main.cpp
+++ CPP_07/ex01/main.cpp
@@ -5,6 +5,15 @@ int main(void) {
 	int size = 3;
 
 	::iter(arr, size, printElement<int>);
+	std::cout << "Multiply each element of the int array by 2:" << std::endl;
+	::iter(arr, size, multiplyElement);
+	::iter(arr, size, printElement<int>);
+
+	char array[] = {'a', 'b', 'c'};
+	// int size = 3;
+	std::cout << "Array of chars:" << std::endl;
+	::iter(array, size, printElement);
+	::iter(array, size, toUpper);
 
 	return 0;
 }
