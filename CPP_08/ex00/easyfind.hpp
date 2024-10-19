5b78b4e6510544aa5dd0a223680e61f72bb6eaf4
MemoCSales <memocsales@gmail.com>
2024-10-19 17:25:23 +0200

    mostly finishing the ex00

--- CPP_08/ex00/easyfind.hpp
+++ CPP_08/ex00/easyfind.hpp
@@ -1,6 +1,10 @@
 #pragma once
 # include <iostream>
 # include <exception>
+# include <cstdlib>
+# include <algorithm>
+# include <iomanip>
+
 
 # define RED		"\033[1;31m"
 # define GREEN		"\033[1;32m"
@@ -11,6 +15,20 @@
 # define WHITE		"\033[1;37m"
 # define DEFAULT		"\033[0m"
 
-// template <typename T> void easyfind(T &first, int value) {
 
-// }
+class NoOcurrenceFound : public std::exception {
+	public:
+		virtual const char* what() const throw() {
+			return ("Error: The value has not been found in the container");
+		}
+};
+
+template <typename T> typename T::iterator easyfind(T &first, int value) {
+
+	typename T::iterator it = std::find(first.begin(), first.end(), value);
+	if (it != first.end())
+		return it;
+	else {
+		throw NoOcurrenceFound();
+	}
+}
--- CPP_08/ex00/main.cpp
+++ CPP_08/ex00/main.cpp
@@ -1,20 +1,48 @@
 #include "easyfind.hpp"
 #include <vector>
 
+# define MAX_SIZE 50
+
 int main () {
-	std::vector<int> ejemplo;
+	std::vector<int> ejemplo(MAX_SIZE);
 
-	for (int i = 1; i <= 5; i++)
-	{
-		ejemplo.push_back(i);
-	}
+	srand(static_cast<unsigned int>(time(0)));
+	int min = 1;
+	int max = 100;
 
-	std::cout << "Vector elements are: " << std::endl;
 	for (std::vector<int>::iterator it = ejemplo.begin(); it != ejemplo.end(); it++)
 	{
-		std::cout << *it << " ";
+		*it = min + std::rand() % (max - min + 1);
 	}
+
+	// Table header
+	std::cout << std::left << std::setw(10) << "Index" << std::setw(10) << "Value" << std::endl;
+	std::cout << std::string(20, '-') << std::endl;
+
+	for (std::vector<int>::size_type i = 0; i != ejemplo.size(); i++)
+	{
+        std::cout << std::left << std::setw(5) << i << " ->     " << std::setw(10) << ejemplo[i] << std::endl;
+	}
+
+	// int index = 0;
+
+	// for (std::list<int>::iterator it = ejemplo.begin(); it != ejemplo.end(); ++it, index++)
+	// {
+    //     std::cout << std::left << std::setw(5) << index << " ->     " << std::setw(10) << *it << std::endl;
+	// }
 	
+	
+	try
+	{
+		int value = 42;
+		std::cout << BLUE <<"Occurence to be found: " << DEFAULT << value << std::endl;
+		std::vector<int>::iterator number = easyfind(ejemplo, value);
+		std::cout << *number << ": " GREEN << "found in the container! " << DEFAULT << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << RED << e.what() << DEFAULT << '\n';
+	}
 	
 	return 0;
 }
