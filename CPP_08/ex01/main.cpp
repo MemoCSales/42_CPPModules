1d3a7df656c6bea97741122dd1fd013ae7a0f116
MemoCSales <memocsales@gmail.com>
2024-10-24 10:47:12 +0200

    updating files ex01

--- CPP_08/ex01/main.cpp
+++ CPP_08/ex01/main.cpp
@@ -2,22 +2,22 @@
 
 int main()
 {
-	// try	{
-	// 	Span sp = Span(5);
-	// 	sp.addNumber(6);
-	// 	sp.addNumber(3);
-	// 	sp.addNumber(17);
-	// 	sp.addNumber(9);
-	// 	sp.addNumber(11);
-	// 	// sp.addNumber(42);
-	// 	// sp.printNumbers();
-	// 	std::cout << sp.shortestSpan() << std::endl;
-	// 	std::cout << sp.longestSpan() << std::endl;
-	// 	// sp.printNumbers();
-	// }
-	// catch(const std::exception& e)	{
-		// std::cerr << RED << "Exception: " << DEFAULT << e.what() << '\n';
-	// }
+	try	{
+		Span sp = Span(5);
+		sp.addNumber(6);
+		sp.addNumber(3);
+		sp.addNumber(17);
+		sp.addNumber(9);
+		sp.addNumber(11);
+		// sp.addNumber(42);
+		// sp.printNumbers();
+		std::cout << sp.shortestSpan() << std::endl;
+		std::cout << sp.longestSpan() << std::endl;
+		// sp.printNumbers();
+	}
+	catch(const std::exception& e)	{
+		std::cerr << RED << "Exception: " << DEFAULT << e.what() << '\n';
+	}
 
 	try
 	{
