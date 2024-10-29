173b5eb7d33556ae0d48a192c551fc44c5e87524
Manuel Guillermo Cruz Sales <mcruz-sa@c2r9s8.42wolfsburg.de>
2024-10-29 14:36:59 +0100

    first commit

---
+++ CPP_09/common.mk
@@ -0,0 +1,30 @@
+# Contains common build rules and settings that are shared
+# Compiler and flags
+CPP			 = c++
+CPPFLAGS	 = -std=c++98
+CPPFLAGS	+= -Wall -Wextra -Werror -Wshadow -pedantic
+# CPPFLAGS	+= -fsanitize=address
+CPPFLAGS	+= -g
+
+# Directories
+OBJDIR		= .obj
+
+# Common rules
+$(OBJDIR)/%.o: %.cpp
+	@mkdir -p $(dir $@)
+	@$(CPP) -o "$@" $(CPPFLAGS) "$<" -c
+
+clean:
+	$(RM) $(OBJ)
+	-@rmdir $(OBJDIR) 2>/dev/null || true
+
+fclean: clean
+	$(RM) $(NAME)
+
+re: fclean
+	$(MAKE) $(NAME)
+
+val: 
+	valgrind --leak-check=full ./$(NAME)
+
+.PHONY: all clean fclean re val
