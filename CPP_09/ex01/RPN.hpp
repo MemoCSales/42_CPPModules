4f6eea04a443fbf7a0a233dcceb60939d503c20b
Manuel Guillermo Cruz Sales <mcruz-sa@c2r8s2.42wolfsburg.de>
2024-11-04 13:20:09 +0100

    starting RPN

---
+++ CPP_09/ex01/RPN.hpp
@@ -0,0 +1,68 @@
+#ifndef RPN_HPP
+# define RPN_HPP
+# include <iostream>
+
+# define DEBUG 0
+
+# define RED		"\033[1;31m"
+# define GREEN		"\033[1;32m"
+# define YELLOW		"\033[1;33m"
+# define BLUE		"\033[1;34m"
+# define MAGENT		"\033[1;35m"
+# define CYAN		"\033[1;36m"
+# define WHITE		"\033[1;37m"
+# define DEFAULT		"\033[0m"
+
+// class RPN {
+// 	private:
+	
+// 	public:
+// 		RPN(void);
+// 		RPN("Parameter Constructor");
+// 		RPN(const RPN &other);
+// 		RPN &operator=(const RPN &other);
+// 		~RPN();
+// };
+
+#endif
+
+// #include "RPN.hpp"
+
+// // ++++Constructor
+// RPN::RPN(void) {
+// 	if (DEBUG) {
+// 		std::cout << "RPN default constructor called" << std::endl;
+// 	}
+// }
+
+// // ++++Constructor
+// RPN::RPN("Parameters") {
+// 	if (DEBUG) {
+// 		std::cout << "RPN parameter constructor called" << std::endl;
+// 	}
+// }
+
+// // Copy Constructor
+// RPN::RPN(const RPN &other) : "_variable(other._variable)"{
+// 	if (DEBUG) {
+// 		std::cout << "RPN copy constructor called" << std::endl;
+// 	}
+// }
+
+// // Assignment Operator
+// RPN &RPN::operator=(const RPN &other) {
+// 	if (DEBUG) {
+// 		std::cout << "RPN Assignment Operator called" << std::endl;
+// 	}
+// 	if (this != &other) {
+// 		// Copy data from other to this
+// 	}
+// 	return *this;
+// }
+
+// // ----Destructor
+// RPN::~RPN() {
+// 	if (DEBUG) {
+// 		std::cout << "RPN destructor called" << std::endl;
+// 	}
+// }
---
+++ CPP_09/ex01/main.cpp
@@ -0,0 +1,9 @@
+# include "RPN.hpp"
+
+int main(int argc, char** argv) {
+	(void)argc;
+	(void)argv;
+
+	std::cout << "main function yay" << std::endl;
+	return 0;
+}
