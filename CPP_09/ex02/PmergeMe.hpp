9252b483266dd5f9053cd8c09d4a8e91bd71b4b8
MemoCSales <memocsales@gmail.com>
2024-12-28 15:03:09 +0100

    starting over

--- CPP_09/ex02/PmergeMe.hpp
+++ CPP_09/ex02/PmergeMe.hpp
@@ -21,47 +21,6 @@
 typedef std::deque<int>::const_iterator itDeque;
 typedef std::vector<int>::const_iterator itVector;
 
-
-
-// Type trait to determine the appropriate type of container for the pairs
-
-/* There traits use template specialization to determine if a given type
- is a vector or deque */
- template <typename T>
- struct isVector {
-	static const bool value = false;
- };
-
- template <typename T>
- struct isVector<std::vector<T> > {
-	static const bool value = true;
- };
-
-  template <typename T>
- struct isDeque {
-	static const bool value = false;
- };
-
- template <typename T>
- struct isDeque<std::deque<T> > {
-	static const bool value = true;
- };
-
- template <typename T, bool itIsVector = isVector<T>::value, bool itIsDeque = isDeque<T>::value>
- struct pairContainer;
-
-/* This trait uses SFINAE to select appropriate container type based on the results
-of the isVector and isDeque traits */
- template <typename T>
- struct pairContainer<T, true, false> {
-	typedef std::vector<std::pair<typename T::value_type, typename T::value_type> > type;
- };
-
- template <typename T>
- struct pairContainer<T, false, true> {
-	typedef std::deque<std::pair<typename T::value_type, typename T::value_type> > type;
- };
-
 class PmergeMe {
 	private:
 		std::vector<int> _myVector;
@@ -84,19 +43,6 @@ class PmergeMe {
 		// Template Methods
 		template <typename T>
 		static void printContainer(const T& container);
-		template <typename T>
-		static void printPairwiseContainer(const typename pairContainer<T>::type& container);
-		template <typename T>
-		void insertionSort(T& container);
-		template <typename T>
-		void insertionSortRecursive(T& container, int n);
-		template <typename T>
-		void separatePairs(typename pairContainer<T>::type&, T&, T&);
-		template <typename T>
-		void fordJohnsonAlgo(T& container);
-		template <typename T>
-		typename pairContainer<T>::type pairWise(const T& container);
-
 };
 
 # include "PmergeMe.tpp"
--- CPP_09/ex02/main.cpp
+++ CPP_09/ex02/main.cpp
@@ -9,28 +9,21 @@ int main(int argc, char **argv) {
 	}
 	
 	/* printing Deque*/
-	std::cout << "Deque: ";
-	for (itDeque it = obj.getDeque().begin(); it != obj.getDeque().end(); it++) {
-		std::cout << *it << " ";
-	}
+	// std::cout << "Deque: ";
+	// for (itDeque it = obj.getDeque().begin(); it != obj.getDeque().end(); it++) {
+	// 	std::cout << *it << " ";
+	// }
 
-	/* printing Vector */
-	std::cout << "\nVector: ";
-	for (itVector it = obj.getVector().begin(); it != obj.getVector().end(); it++) {
-		std::cout << *it << " ";
-	}
-	std::cout << std::endl;
+	// /* printing Vector */
+	// std::cout << "\nVector: ";
+	// for (itVector it = obj.getVector().begin(); it != obj.getVector().end(); it++) {
+	// 	std::cout << *it << " ";
+	// }
+	// std::cout << std::endl;
 	
-	// obj.fordJohnsonAlgo(obj.getDeque());
-	// obj.fordJohnsonAlgo(obj.getVector());
-	// obj.insertionSort(obj.getDeque());
-	// std::cout << "Print deque: " << std::endl;
-	// PmergeMe::printContainer(obj.getDeque());
-	// obj.insertionSort(obj.getVector());
-	// std::cout << "Print vector: " << std::endl;
-	// PmergeMe::printContainer(obj.getVector());
-	// std::cout << "Pairwise Deque: " << std::endl;
-	// PmergeMe::printPairwiseContainer<std::deque<int> >(obj.pairWise(obj.getDeque()));
-
+	std::cout << "Print deque: " << std::endl;
+	PmergeMe::printContainer(obj.getDeque());
+	std::cout << "Print vector: " << std::endl;
+	PmergeMe::printContainer(obj.getVector());
 	return 0;
 }
